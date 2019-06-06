35bc03fc3784969e8d7599b0b6b0fe36206f780d
Na Hyung Choi and Tamara Trefilova <tamara@>
2019-06-06 14:23:14 -0700

    validation states do work

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,14 @@ $(function() {
   var inputColor = $(".color").val();
   var age = parseInt($("input.age").val());
   // alert("age="+age+"color="+inputColor);
-  if(age<21 && inputColor==="red"){
-    $(".justin").show();
-  } else if (age>=21 && inputColor==="blue"){
-    $(".brad").show();
-  } else if(age>=45 && inputColor==="green"){
-    $(".katy").show();
-  } else {
-    $(".katy, .brad").show();
-  }
+  // if(age<21 && inputColor==="red"){
+  //   $(".justin").show();
+  // } else if (age>=21 && inputColor==="blue"){
+  //   $(".brad").show();
+  // } else if(age>=45 && inputColor==="green"){
+  //   $(".katy").show();
+  // } else {
+  //   $(".katy, .brad").show();
+  // }
   });
 });
