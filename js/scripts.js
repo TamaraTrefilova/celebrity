43392355afe30789d1d56ed7cb5cb5247d5c99ac
Na Hyung Choi and Tamara Trefilova <tamara@>
2019-06-06 11:59:05 -0700

    add UI

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+  $("form").submit((event) {
+  event.preventDefault();
+
+  var inputName = $("input#name").val();
+  var inputAge = $("input#age").val();
+  var inputColor = $("input#color").val();
+
+  if(inputAge === false || !inputName )
+
+
+  });
+});
